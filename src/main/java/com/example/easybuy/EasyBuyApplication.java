7a1336b690ec7fba1d7ce29f3d212ccec7886911
fenineo <1418294357@qq.com>
2021-07-20 21:39:24 +0800

    Initial commit

---
+++ src/main/java/com/example/easybuy/EasyBuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.easybuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBuyApplication.class, args);
+    }
+
+}
