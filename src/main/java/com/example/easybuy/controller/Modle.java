e1615a1b056cb9c81b5f1b0501f2a496a1f195b3
596118372 <87698199+596118372@users.noreply.github.com>
2021-08-01 17:58:06 +0800

    商品
    商品

--- src/main/java/com/example/easybuy/controller/Modle.java
+++ src/main/java/com/example/easybuy/controller/Modle.java
@@ -44,7 +44,6 @@ public class Modle {
                                  @RequestParam("categoryLevel3Id") Integer categoryLevel3Id,
                                  @RequestParam("acth") MultipartFile acth,
                                  HttpServletRequest request) throws IOException, SftpException {
-        System.out.println(name);
         Product product=new Product();
         product.setName(name);
         product.setPrice(price);
@@ -57,7 +56,7 @@ public class Modle {
 //      product.setFileName(fileName);
 
         //连接
-        SftpUtil sftp = new SftpUtil("fnztion", "123456", "192.168.1.193", 22);
+        SftpUtil sftp = new SftpUtil("fnztion", "123456", "192.168.137.192", 22);
         //登录
         sftp.login();
         //输出到服务器特定目录
@@ -96,19 +95,40 @@ public class Modle {
      * 修改
      */
     @RequestMapping("/modifyProduct")
-    public String modifyProduct(String id,String name,String price,String stock,String fileName,String categoryLevel1Id,String categoryLevel2Id,String categoryLevel3Id){
+    public String modifyProduct(@RequestParam("name") String name,
+                                @RequestParam("id") String id,
+                                @RequestParam("price") Integer price,
+                                @RequestParam("stock") Integer stock,
+                                @RequestParam("categoryLevel1Id") Integer categoryLevel1Id,
+                                @RequestParam("categoryLevel2Id") Integer categoryLevel2Id,
+                                @RequestParam("categoryLevel3Id") Integer categoryLevel3Id,
+                                @RequestParam("acth") MultipartFile acth,
+                                HttpServletRequest request) throws IOException, SftpException {
+
+        //取名字给图片
+        String fileNames = System.currentTimeMillis() + RandomUtils.nextInt(0, 10000) + "_Personal.jpg";
+//      product.setFileName(fileName);
+        //连接
+        SftpUtil sftp = new SftpUtil("fnztion", "123456", "192.168.137.192", 22);
+        //登录
+        sftp.login();
+        //输出到服务器特定目录
+        sftp.upload("/home/fnztion/apache-tomcat-8.5.69/webapps/img/zxc", fileNames, acth.getInputStream());
+        //退出
+        sftp.logout();
         Product product=new Product();
-        product.setId(id);
         product.setName(name);
-        product.setPrice(Integer.parseInt(price));
-        product.setStock(Integer.parseInt(stock));
-        product.setCategoryLevel1Id(Integer.parseInt(categoryLevel1Id));
-        product.setCategoryLevel2Id(Integer.parseInt(categoryLevel2Id));
-        product.setCategoryLevel3Id(Integer.parseInt(categoryLevel3Id));
-        product.setFileName(fileName);
+        product.setPrice(price);
+        product.setStock(stock);
+        product.setCategoryLevel1Id(categoryLevel1Id);
+        product.setCategoryLevel2Id(categoryLevel2Id);
+        product.setCategoryLevel3Id(categoryLevel3Id);
+        product.setFileName(fileNames);
+        product.setId(id);
+        System.out.println(product);
         boolean flag=productService.modifyProduct(product);
         System.out.println(flag);
-        return null;
+        return JSON.toJSONString(flag);
     }
     /**
      * 商品的逻辑删除
